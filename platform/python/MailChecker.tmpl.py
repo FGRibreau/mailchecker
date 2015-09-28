7be778d3e3acf9dc70cd7fdbe50a79fe21c68c48
Herman Slatman <hermanslatman@hotmail.com>
2015-09-29 00:31:40 +0200

    Simple python drop-in implementation

---
+++ platform/python/MailChecker.tmpl.py
@@ -0,0 +1,22 @@
+import re
+
+class MailChecker(object):
+
+    def __init__(self):
+        self.list_string = """{{list}}"""
+
+        # valid email format regex source: https://gist.github.com/dideler/5219706
+        self.email_regex = """([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`"
+                    "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|"
+                    "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"""
+        self.fake_matcher = re.compile('|'.join(map(lambda x: '\\b' + x.lower() + '$', self.list_string.split(","))))
+        self.valid_matcher = re.compile(self.email_regex)
+
+    def is_valid(self, email):
+        return self.is_valid_email_format(email) and self.fake_matcher.search(email) == None
+
+    def is_valid_email_format(self, email):
+        if email:
+            return self.valid_matcher.search(email) != None
+        else:
+			return False
