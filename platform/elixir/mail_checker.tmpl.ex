9576a4c9975f94a32415617d979e08fff2672878
Owen Stephens <owen@owenstephens.co.uk>
2016-03-14 22:54:34 +0000

    Consider all domain suffixes in Elixir

--- platform/elixir/mail_checker.tmpl.ex
+++ platform/elixir/mail_checker.tmpl.ex
@@ -1,28 +1,25 @@
 defmodule MailChecker do
+  @blacklist Enum.into([{{& listSTR }}], HashSet.new)
+
+  def blacklist, do: @blacklist
+
   def valid?(email) do
     valid_address?(email) && !in_blacklist?(email)
   end
 
   def in_blacklist?(email) do
-    set = Enum.into(blacklist, HashSet.new)
-
-    member = HashSet.member?(set, extract_top_domain(email))
-    member
+    Enum.any?(extract_domain_suffixes(email), fn domain -> HashSet.member?(blacklist, domain) end)
   end
 
-  def blacklist do
-    [{{& listSTR }}]
-  end
-
-  def extract_top_domain(email) do
+  def extract_domain_suffixes(email) do
     domain          = String.split(email, "@") |> List.last
     domain_parts    = String.split(domain, ".")
-    top_domain_part = Enum.slice(domain_parts, -2..2)
-    Enum.join(top_domain_part, ".")
+    range           = Range.new(0, (length domain_parts) - 1)
+
+    Enum.map(range, fn n -> Enum.join(Enum.drop(domain_parts, n), ".") end)
   end
 
   def valid_address?(email) do
     Regex.match?(~r/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/i, email)
   end
-
 end
