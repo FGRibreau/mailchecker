2060cda5c8d4205b4be4e764607bd53b49adad4c
Owen Stephens <owen@owenstephens.co.uk>
2016-03-15 23:45:34 +0000

    Improve Python implmentation and tests
    - Use the (better) regex from the Ruby code for checking for well-formed
      email addresses,
    - Check for matches using identity equality (is/is not None),
    - Use stdlib unittest module for tests and import testcases from Ruby

--- platform/python/MailChecker.tmpl.py
+++ platform/python/MailChecker.tmpl.py
@@ -5,19 +5,18 @@ class MailChecker(object):
     def __init__(self):
         self.list_string = """{{list}}"""
 
-        # valid email format regex source: https://gist.github.com/dideler/5219706
-        self.email_regex = """([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`"
-                    "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|"
-                    "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"""
+        # Based on PHP FILTER_VALIDATE_EMAIL
+        # See https://fightingforalostcause.net/content/misc/2006/compare-email-regex.php
+        self.email_regex = r"^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$"
         self.fake_matcher = re.compile('|'.join(map(lambda x: '\\b' + x.lower() + '$', self.list_string.split(","))))
         self.valid_matcher = re.compile(self.email_regex)
 
     def is_valid(self, email):
         email = email.lower()
-        return self.is_valid_email_format(email) and self.fake_matcher.search(email) == None
+        return self.is_valid_email_format(email) and self.fake_matcher.search(email) is None
 
     def is_valid_email_format(self, email):
         if email and email != '':
-            return self.valid_matcher.search(email) != None
+            return self.valid_matcher.search(email) is not None
         else:
             return False
