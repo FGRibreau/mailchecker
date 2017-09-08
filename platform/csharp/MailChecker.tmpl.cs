2b223ea33823b1d56e6b5b356ae756e0e1575356
Oleh Maksymets <oleh.maksymets@agenatrader.com>
2017-09-08 16:25:19 +0300

    Add C# Implementation

---
+++ platform/csharp/MailChecker.tmpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MailChecker
+{
+    public static class MailChecker
+    {
+        private static readonly Regex EmailValidator = new Regex({{&unanchoredRegexpString}});
+        private static readonly string[] Blacklist = {{{&listSTR}}};
+		
+        public static bool IsValid(string email)
+        {
+            email = email.ToLower();
+            return EmailValidator.IsMatch(email) && !IsBlacklisted(email);
+        }
+
+        private static bool IsBlacklisted(string email)
+        {
+            return AllDomainSuffixes(email).Any(Blacklist.Contains);
+        }
+
+        private static IEnumerable<string> AllDomainSuffixes(string email)
+        {
+            var domainComponents = email.Split('@')[1].Split('.');
+
+            string tmpDomain = null;
+            return domainComponents.Reverse().Select(domain => tmpDomain = tmpDomain != null ? String.Join(".", domain, tmpDomain) : domain);
+        }
+    }
+}
