04d147afab602ed3dc0c0fc86b4e0a36d67667d3
Jacek Bzdak <jbzdak@gmail.com>
2018-03-23 15:42:01 +0100

    Added support for python3.

--- platform/python/setup.py
+++ platform/python/setup.py
@@ -15,6 +15,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
   ],
