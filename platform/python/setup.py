4aa3bd3bc415ccaff5011e044e5df81624fae417
François-Guillaume RIBREAU <github@fgribreau.com>
2018-05-14 21:30:24 +0200

    feat(python): updated to 3.2.5

--- platform/python/setup.py
+++ platform/python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name='mailchecker',
-  version = "3.2.4",
+  version = "3.2.5",
   py_modules=['MailChecker'],
   url='https://github.com/FGRibreau/mailchecker',
   license='MIT',
