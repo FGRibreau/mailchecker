4dfd7ae48be1550a9549bb62d9ec7d9653d3342e
Grégoire Pineau <lyrixx@lyrixx.info>
2019-05-22 20:22:03 +0200

    Rewrite a bit the PHP implementation

--- test/platform.php.test.php
+++ test/platform.php.test.php
@@ -1,24 +1,29 @@
 <?php
-# Run tests from the repository root directory:
-# $ composer install && ./vendor/bin/phpunit test/platform.php.test.php
 
-require(__DIR__.'/../platform/php/MailChecker.php');
+// Run tests from the repository root directory:
+// $ composer install && ./vendor/bin/phpunit test/platform.php.test.php
 
-class MailCheckerTest extends PHPUnit_Framework_TestCase
+require __DIR__.'/../platform/php/MailChecker.php';
+
+class MailCheckerTest extends PHPUnit\Framework\TestCase
 {
-    public function assertIsValidResult($expected, $email) {
+    public function assertIsValidResult($expected, $email)
+    {
         $this->assertEquals($expected, MailChecker::isValid($email));
     }
 
-    public function isValid($email) {
+    public function isValid($email)
+    {
         $this->assertIsValidResult(true, $email);
     }
 
-    public function isInvalid($email) {
+    public function isInvalid($email)
+    {
         $this->assertIsValidResult(false, $email);
     }
 
-    public function testReturnTrueIfValidEmail() {
+    public function testReturnTrueIfValidEmail()
+    {
         $this->isValid('plop@plop.com');
         $this->isValid('my.ok@ok.plop.com');
         $this->isValid('my+ok@ok.plop.com');
@@ -27,34 +32,42 @@ class MailCheckerTest extends PHPUnit_Framework_TestCase
         $this->isValid('ok@hotmail.com');
     }
 
-    public function testReturnFalseIfEmailInvalid() {
+    public function testReturnFalseIfEmailInvalid()
+    {
         $this->isInvalid('');
         $this->isInvalid('  ');
         $this->isInvalid('plopplop.com');
         $this->isInvalid('my+ok@ok=plop.com');
         $this->isInvalid('my,ok@ok.plop.com');
-        $this->isInvalid("  ok@gmail.com  ");
-        $this->isInvalid("  ok@gmail.com");
-        $this->isInvalid("ok@gmail.com  ");
+        $this->isInvalid('  ok@gmail.com  ');
+        $this->isInvalid('  ok@gmail.com');
+        $this->isInvalid('ok@gmail.com  ');
         $this->isInvalid("\nok@gmail.com\n");
         $this->isInvalid("\nok@gmail.com");
         $this->isInvalid("ok@gmail.com\n");
     }
 
-    public function testReturnFalseIfThrowableDomain() {
+    public function testReturnFalseIfThrowableDomain()
+    {
         $this->isInvalid('ok@tmail.com');
         $this->isInvalid('ok@33mail.com');
         //$this->isInvalid('ok@ok.33mail.com');
         $this->isInvalid('ok@guerrillamailblock.com');
     }
 
-    public function testReturnFalseForBlacklistedDomainsAndTheirSubdomains() {
-        foreach(MailChecker::blacklist() as $blacklisted_domain) {
-            $this->isInvalid("test@" . $blacklisted_domain);
-            $this->isInvalid("test@subdomain." . $blacklisted_domain);
-            # Should not be invalid as a subdomain of a valid domain.
-            $this->isValid("test@" . $blacklisted_domain . ".gmail.com");
+    public function provideBlackListTests()
+    {
+        foreach (MailChecker::blacklist() as $blacklistedDomain) {
+            yield [$blacklistedDomain];
         }
     }
+
+    /** @dataProvider provideBlackListTests */
+    public function testReturnFalseForBlacklistedDomainsAndTheirSubdomains($blacklistedDomain)
+    {
+        $this->isInvalid('test@'.$blacklistedDomain);
+        $this->isInvalid('test@subdomain.'.$blacklistedDomain);
+        // Should not be invalid as a subdomain of a valid domain.
+        $this->isValid('test@'.$blacklistedDomain.'.gmail.com');
+    }
 }
-?>
