fabac1fc33132e4d74c6081968872d045fe4708e
François-Guillaume RIBREAU <github@fgribreau.com>
2019-12-06 23:04:59 +0100

    feat(python): updated to 3.3.3

--- platform/python/setup.py
+++ platform/python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name='mailchecker',
-  version = "3.3.2",
+  version = "3.3.3",
   py_modules=['MailChecker'],
   url='https://github.com/FGRibreau/mailchecker',
   license='MIT',
