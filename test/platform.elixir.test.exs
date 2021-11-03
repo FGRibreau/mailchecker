f09dceb17feea3eb7835075bcbb42b2115a360c2
Lawrence Chou <lawrencechou1024@gmail.com>
2021-11-03 22:19:45 +0800

    feat(elixir): add add_custom_domains

--- test/platform.elixir.test.exs
+++ test/platform.elixir.test.exs
@@ -25,6 +25,7 @@ defmodule MailCheckerTest do
   end
 
   test "should return true if the email is valid" do
+    {:ok, _pid} = MailChecker.start_link() # FIXME: This is a breaking change :(
     assert_valid("plop@plop.com")
     assert_valid("my.ok@ok.plop.com")
     assert_valid("my+ok@ok.plop.com")
@@ -47,6 +48,7 @@ defmodule MailCheckerTest do
   end
 
   test "should return false if the email come from a throwable domain" do
+    {:ok, _pid} = MailChecker.start_link() # FIXME: This is a breaking change :(
     assert_invalid("ok@tmail.com")
     assert_invalid("ok@33mail.com")
     assert_invalid("ok@ok.33mail.com")
@@ -54,10 +56,24 @@ defmodule MailCheckerTest do
   end
 
   test "should return false if the email is from a blacklisted domain" do
+    {:ok, _pid} = MailChecker.start_link() # FIXME: This is a breaking change :(
     Enum.each(MailChecker.blacklist, fn domain ->
         assert_invalid("test@" <> domain)
         assert_invalid("test@subdomain." <> domain)
         assert_valid("test@" <> domain <> ".gmail.com")
     end)
+  end
+
+  test "should return false if the email is from a custom domain" do
+    {:ok, _pid} = MailChecker.start_link()
+    assert_valid("foo@youtube.com")
+    assert_valid("foo@google.com")
+    assert_valid("ok@gmail.com")
+
+    MailChecker.add_custom_domains(["youtube.com", "google.com"])
+
+    assert_invalid("foo@youtube.com")
+    assert_invalid("foo@google.com")
+    assert_valid("ok@gmail.com")
   end
 end
