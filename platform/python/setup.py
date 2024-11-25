0f417468492f10bef0080c146c818e4b4ea758d9
François-Guillaume Ribreau <git@fgribreau.com>
2024-11-25 23:22:35 +0100

    feat(python): updated to 6.0.12 feat(python): updated to 6.0.12

--- platform/python/setup.py
+++ platform/python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name='mailchecker',
-  version = "6.0.11",
+  version = "6.0.12",
   py_modules=['MailChecker'],
   url='https://github.com/FGRibreau/mailchecker',
   license='MIT',
