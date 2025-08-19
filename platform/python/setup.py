fc4c7bb347159ce0a8fb7979a6dc2b79f30757f6
François-Guillaume Ribreau <git@fgribreau.com>
2025-08-19 19:04:27 +0200

    feat(python): updated to 6.0.18

--- platform/python/setup.py
+++ platform/python/setup.py
@@ -1,12 +1,6 @@
 # coding=utf-8
 
 import setuptools
-import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "../../README.md"), "r") as fh:
-  long_description = fh.read()
 
 setuptools.setup(
   name="mailchecker",
@@ -14,8 +8,6 @@ setuptools.setup(
   author='Francois-Guillaume Ribreau',
   author_email='github@fgribreau.com',
   description="Cross-language temporary email detection library. Stop users from signing up with temporary email addresses.",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
   url="https://github.com/FGRibreau/mailchecker",
   py_modules=["MailChecker"],
   classifiers=[
