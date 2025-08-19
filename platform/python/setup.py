fcfe3ca27607d5ac704fae340ce35d017a094a13
François-Guillaume Ribreau <git@fgribreau.com>
2025-08-19 18:59:32 +0200

    feat(python): updated to 6.0.18

--- platform/python/setup.py
+++ platform/python/setup.py
@@ -1,23 +1,24 @@
 # coding=utf-8
 
-from setuptools import setup
+import setuptools
 
-setup(
-  name='mailchecker',
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="mailchecker",
   version = "6.0.18",
-  py_modules=['MailChecker'],
-  url='https://github.com/FGRibreau/mailchecker',
-  license='MIT',
   author='Francois-Guillaume Ribreau',
   author_email='github@fgribreau.com',
-  description='Cross-language email validation. '
-              'Backed by a database of thousands throwable email providers.',
+  description="Cross-language temporary email detection library. Stop users from signing up with temporary email addresses.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/FGRibreau/mailchecker",
+  py_modules=["MailChecker"],
   classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
   ],
+  python_requires='>=3.6',
 )
